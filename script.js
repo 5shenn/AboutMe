5c4446164ef50b911bd63f89e6a6a47e39da5bbd
shen <142518601+5shenn@users.noreply.github.com>
2024-10-01 11:06:44 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  function copyText() {
+    // Select the text
+    const text = document.getElementById("discord").innerText;
+
+    // Copy the text to the clipboard
+    navigator.clipboard.writeText(text).then(function() {
+        alert('Text copied to clipboard: ' + text);
+    }).catch(function(err) {
+        console.error('Error copying text: ', err);
+    });
+}
